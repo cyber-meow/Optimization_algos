8fdf8a697b0699f86fbeb98f4f5acd42a73d7d3a
cyber-meow <sjungle305@gmail.com>
2017-11-12 13:13:27 +0100

    SVM newton

---
+++ SVM/barrier.jl
@@ -0,0 +1,53 @@
+# Solve the SVM classification problem using the barrier method.
+
+function svmcentering(
+    X::AbstractMatrix, Y::AbstractVector, C::Real,
+    w::AbstractVector, z::AbstractVector, t::Real;
+    ϵ::Real=1e-10, α::Real=0.01, β::Real=0.05)
+  A = [-Y.*X (-eye(size(Y, 1))); zeros(X) (-eye(size(Y, 1)))]
+  b = [-1.*ones(Y); zeros(Y)]
+  w, z = copy(w), copy(z)
+  x = [w; z]
+  f(w, z) = t*(norm(w)^2/2+C*ones(Y)⋅z)-sum(log.(Y.*(X*w)+z-1)+log.(z))
+  fx(x) = f(x[1: size(w, 1)], x[size(w, 1)+1: end])
+  numsteps = 0
+  while true
+    d = 1./(b-A*x)
+    ∇ = t*[w; C*ones(Y)]+A'*d
+    H = t*Diagonal([ones(w); zeros(Y)])+A'*Diagonal(d)^2*A
+    Δxnt = -H\∇
+    λ² = -∇⋅Δxnt
+    λ²/2 < ϵ && break
+    tbacktrace = 1
+    xtest = x + tbacktrace*Δxnt
+    while(any(A*xtest .≥ b) || fx(xtest) > fx(x)-tbacktrace*α*λ²)
+      tbacktrace *= β
+      xtest = x + tbacktrace*Δxnt
+    end
+    assert(fx(xtest) <= fx(x))
+    # numeric limit
+    fx(xtest) == fx(x) && break
+    x = xtest
+    w, z = x[1: size(w, 1)], x[size(w, 1)+1: end]
+    numsteps += 1
+  end
+  w, z, numsteps
+end
+
+function svmbarrier(
+    X::AbstractMatrix, Y::AbstractVector, C::Real, ϵ::Real;
+    t0::Real=1., μ::Real=100., α::Real=0.01, β::Real=0.05)
+  w = zeros(size(X, 2))
+  z = ones(Y)*2
+  m = float(2*size(Y, 1))
+  t = t0
+  numstepsarray::Vector{Int} = []
+  while true
+    w, z, numsteps = svmcentering(X, Y, C, w, z, t; α=α, β=β)
+    push!(numstepsarray, numsteps)
+    m/t < ϵ && break
+    t *= μ
+  end
+  αdual = 1./(t*Y.*X*w+z-1)
+  w, αdual, numstepsarray
+end
---
+++ SVM/common.jl
@@ -0,0 +1,37 @@
+# Some basic functions to interact with the data.
+
+function gencov(d::Integer)
+  A = rand(d, d)
+  eye(d) + (A + A')/2
+end
+
+function plotclouds(cloudA, cloudB)
+  scatter(cloudA[1,:], cloudA[2,:], label="-1")
+  scatter!(cloudB[1,:], cloudB[2,:], label="1")
+end
+
+function errorrate(classA::MvNormal, classB::MvNormal,
+                   w::AbstractVector, n::Integer)
+  testA = rand(classA, n)
+  testB = rand(classB, n)
+  X = [[testA testB]' ones(2*n)]
+  Y = [-1.*ones(n); ones(n)]
+  sum(sign.(X*w).≠Y)/n
+end
+
+function drawborder(f)
+  xs = linspace(xlims()..., 100)
+  ys = linspace(ylims()..., 100)
+  contour!(xs, ys, f, levels=[0], colorbar=false, ls=:dash)
+end
+
+function plotdualitygap(m::Integer, μ::Real,
+                        numstepsarray::AbstractVector{T}) where T<:Integer
+  dualgaps = [m/μ^i for i=0:length(numstepsarray)]
+  numstepssumsarray = reduce(
+    (arr, n) -> [arr; arr[end]+n] ,[0], numstepsarray)
+  dualgaps2 = repeat(dualgaps, inner=2)[1:end-1]
+  numstepssumsarray2 = repeat(numstepssumsarray, inner=2)[2:end]
+  plot(numstepssumsarray2, dualgaps2, yscale=:log10,
+       xlabel="Newton iterations", ylabel="duality gap")
+end
